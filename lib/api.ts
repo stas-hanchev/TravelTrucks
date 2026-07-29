683e2bc7d9648930d4a3a04dc5aaf0aed2128bd9
Stanislav <stas.hanchev@gmail.com>
2026-07-29 18:18:05 +0300

    fix

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-//https://campers-api.goit.study/docs#/
 const API_URL =
-  process.env.NEXT_PUBLIC_CAMPERS_API_URL ??
-  "https://campers-api.goit.study";
+  process.env.NEXT_PUBLIC_CAMPERS_API_URL;
 
 export const api = axios.create({
   baseURL: API_URL,
